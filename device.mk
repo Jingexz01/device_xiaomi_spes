7a77a14ff9e3aed5dd0d3ffad33a4cb08fcc91fe
Jingexz01 <jingexz01@gmail.com>
2026-09-12 00:14:11 +0000

    spes/spesn: Fixed inconsistencies.

--- device.mk
+++ device.mk
@@ -232,14 +232,26 @@ TARGET_EXCLUDES_AUDIOFX := true
 $(call soong_config_set,libinit,vendor_init_lib,init_xiaomi_spes)
 
 #Health
-$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
+#$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
+#$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+#$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+
+#$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
+#$(call soong_config_set_bool,lineage_health,charging_control_supports_deadline,false)
+#$(call soong_config_set_bool,lineage_health,charging_control_supports_limit,false)
+#$(call soong_config_set_bool,lineage_health,charging_control_supports_toggle,false)
+
+#Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/batt_slate_mode)
 $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
-
 $(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
-$(call soong_config_set_bool,lineage_health,charging_control_supports_deadline,false)
-$(call soong_config_set_bool,lineage_health,charging_control_supports_limit,false)
-$(call soong_config_set_bool,lineage_health,charging_control_supports_toggle,false)
+$(call soong_config_set,lineage_health,fast_charge_node,/sys/class/sec/switch/afc_disable)
+$(call soong_config_set,lineage_health,fast_charge_value_none,1)
+$(call soong_config_set,lineage_health,fast_charge_value_fast_charge,0)
 
 # DRM
 PRODUCT_PACKAGES += \
