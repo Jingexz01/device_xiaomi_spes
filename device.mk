9b72e350ffe11a1294ab66080feca1fe2986aa08
Jingexz01 <jingexz01@gmail.com>
2026-09-13 15:14:30 +0000

    spes/spesn: Backup 09122026

--- device.mk
+++ device.mk
@@ -252,7 +252,10 @@ $(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,fal
 $(call soong_config_set,lineage_health,fast_charge_node,/sys/class/sec/switch/afc_disable)
 $(call soong_config_set,lineage_health,fast_charge_value_none,1)
 $(call soong_config_set,lineage_health,fast_charge_value_fast_charge,0)
-
+$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
+$(call soong_config_set_bool,lineage_health,charging_control_supports_deadline,false)
+$(call soong_config_set_bool,lineage_health,charging_control_supports_limit,false)
+$(call soong_config_set_bool,lineage_health,charging_control_supports_toggle,false)
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
